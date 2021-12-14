a1cb4d42939ecef43525cbca8f2dd3883c8dbcfb
Dror Kessler <dror27.kessler@gmail.com>
2021-12-14 12:44:00 +0000

    tm tag refactored into methods

--- src/main/java/picard/sam/markduplicates/util/AbstractMarkDuplicatesCommandLineProgram.java
+++ src/main/java/picard/sam/markduplicates/util/AbstractMarkDuplicatesCommandLineProgram.java
@@ -250,7 +250,7 @@ public abstract class AbstractMarkDuplicatesCommandLineProgram extends AbstractO
     private static boolean isUnpairedReadKnownFragment(final SAMRecord rec) {
         if ( rec.getReadUnmappedFlag() )
             return false;
-        else if ( MarkDuplicates.tmTagContains(rec,MarkDuplicates.TM_TAG_CONTAINS_A) )
+        else if ( MarkDuplicates.isAdapterClipped(rec) )
             return true;
         else if ( !rec.getReadNegativeStrandFlag() )
             return rec.getEnd() != rec.getUnclippedEnd();
