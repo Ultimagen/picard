1fb15097dad69b12659fd105108d5b9654ef7928
Dror Kessler <dror27.kessler@gmail.com>
2022-01-11 14:39:50 +0000

    parameter renamed to reflect flows skipage

--- src/main/java/picard/sam/markduplicates/MarkDuplicates.java
+++ src/main/java/picard/sam/markduplicates/MarkDuplicates.java
@@ -236,11 +236,11 @@ public class MarkDuplicates extends AbstractMarkDuplicatesCommandLineProgram {
 
     @Argument(doc = "Maximal difference of the read end position that counted as equal. Useful for flow based " +
             "reads where the end position might vary due to sequencing errors. Default 0.")
-    public int FLOW_END_POS_UNCERTAINTY = 0;
+    public int UNPAIRED_END_UNCERTAINTY = 0;
 
     @Argument(doc = "Skip first N flows, when considering duplicates. Useful for flow based reads where sometimes there " +
             "is noise in the first flows. Default 0.")
-    public int FLOW_SKIP_START_HOMOPOLYMERS = 0;
+    public int FLOW_SKIP_FIRST_N_FLOWS = 0;
 
     @Argument(doc = "Treat position of read trimming based on quality as the known end (relevant for flow based reads). Default false - if the read " +
             "is trimmed on quality its end is not defined and the read is duplicate with any read starting at the same place. ")
@@ -703,7 +703,7 @@ public class MarkDuplicates extends AbstractMarkDuplicatesCommandLineProgram {
         }
         else if (USE_END_IN_UNPAIRED_READS) {
             ends.read1Coordinate2 = getReadEndCoordinate(rec, rec.getReadNegativeStrandFlag(), false);
-            ends.read1Coordinate2Uncertainty = FLOW_END_POS_UNCERTAINTY;
+            ends.read1Coordinate2Uncertainty = UNPAIRED_END_UNCERTAINTY;
         }
 
         if ( ends.score == 0 ) {
@@ -1145,10 +1145,10 @@ public class MarkDuplicates extends AbstractMarkDuplicatesCommandLineProgram {
 
         if ( !flowOrder.isValid() ) {
             return unclippedCoor;
-        } else if ( certain && FLOW_SKIP_START_HOMOPOLYMERS != 0 ) {
+        } else if ( certain && FLOW_SKIP_FIRST_N_FLOWS != 0 ) {
             final byte[] bases = rec.getReadBases();
             byte        hmerBase = start ? bases[0] : bases[bases.length - 1];
-            int         hmersLeft = FLOW_SKIP_START_HOMOPOLYMERS;      // number of hmer left to trim
+            int         hmersLeft = FLOW_SKIP_FIRST_N_FLOWS;      // number of hmer left to trim
 
             // advance flow order to base
             while ( flowOrder.current() != hmerBase ) {
@@ -1399,9 +1399,8 @@ public class MarkDuplicates extends AbstractMarkDuplicatesCommandLineProgram {
         // if flow mode not specified, make sure no other flow mode parameter is specified
         if ( !FLOW_MODE ) {
             if ( FLOW_QUALITY_SUM_STRATEGY || USE_END_IN_UNPAIRED_READS || USE_UNPAIRED_CLIPPED_END
-            if ( FLOW_QUALITY_SUM_STRATEGY || FLOW_END_LOCATION_SIGNIFICANT || FLOW_USE_CLIPPED_LOCATIONS
-                        || (FLOW_SKIP_START_HOMOPOLYMERS != 0) || FLOW_Q_IS_KNOWN_END
-                        || (ENDS_READ_UNCERTAINTY != 0) ) {
+                        || (FLOW_SKIP_FIRST_N_FLOWS != 0) || FLOW_Q_IS_KNOWN_END
+                        || (UNPAIRED_END_UNCERTAINTY != 0) ) {
                 throw new IllegalArgumentException("FLOW parameters can only be specified in FLOW_MODE");
             }
         }
