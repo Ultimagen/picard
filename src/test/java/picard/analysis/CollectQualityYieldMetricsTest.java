2155b8cb2af6dd462ff3cd4b504b155908d07f00
Dror Kessler <dror27.kessler@gmail.com>
2022-01-11 11:17:33 +0000

    cosmetic changes #1

--- src/test/java/picard/analysis/CollectQualityYieldMetricsTest.java
+++ src/test/java/picard/analysis/CollectQualityYieldMetricsTest.java
@@ -72,8 +72,8 @@ public class CollectQualityYieldMetricsTest extends CommandLineProgramTest {
         Assert.assertEquals(metrics.PF_Q30_BASES, 3145);
         Assert.assertEquals(metrics.Q20_EQUIVALENT_YIELD, 6497);
         Assert.assertEquals(metrics.PF_Q20_EQUIVALENT_YIELD, 6497);
-        Assert.assertEquals(metrics.RLQ30, 0);
-        Assert.assertEquals(metrics.RLQ25, 0);
+        Assert.assertEquals(metrics.AVG_READ_LENGTH_Q_BELOW_30, 0);
+        Assert.assertEquals(metrics.AVG_READ_LENGTH_Q_BELOW_25, 0);
     }
 
     @Test
@@ -106,7 +106,7 @@ public class CollectQualityYieldMetricsTest extends CommandLineProgramTest {
         Assert.assertEquals(metrics.PF_Q30_BASES, 14786);
         Assert.assertEquals(metrics.Q20_EQUIVALENT_YIELD, 30589);
         Assert.assertEquals(metrics.PF_Q20_EQUIVALENT_YIELD, 30589);
-        Assert.assertEquals(metrics.RLQ30, 14);
-        Assert.assertEquals(metrics.RLQ25, 119);
+        Assert.assertEquals(metrics.AVG_READ_LENGTH_Q_BELOW_30, 14);
+        Assert.assertEquals(metrics.AVG_READ_LENGTH_Q_BELOW_25, 119);
     }
 }
